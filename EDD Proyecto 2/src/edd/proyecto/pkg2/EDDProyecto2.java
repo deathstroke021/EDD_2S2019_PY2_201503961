3d8c87fd20de2be893073b26637d23a8c802955d
Fernando Augusto <thedeathstroke021@gmail.com>
2019-10-30 22:41:30 -0600

    Update: Graphic interface

---
+++ EDD Proyecto 2/src/edd/proyecto/pkg2/EDDProyecto2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.proyecto.pkg2;
+
+/**
+ *
+ * @author Fernando Armira
+ */
+public class EDDProyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Login log = new Login();
+        log.setVisible(true);
+        
+    }
+    
+}
